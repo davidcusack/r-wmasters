e315de5310a3e82442fbb5301d905fbd6519914d
Cusack <David.Cusack@gov.bc.ca>
2019-11-30 16:04:05 -0800

    Added Top Ten Academies Bar Plot

--- ggplot-v1-1.R
+++ ggplot-v1-1.R
@@ -1,8 +1,9 @@
 require(dplyr)
 require(ggplot2)
+
 require(hrbrthemes)
 require(viridis)
-
+install.packages('farver')
 
 byacademy<-read.csv('byacademy.csv', header = TRUE, sep =',', stringsAsFactors = FALSE)
 
@@ -20,6 +21,8 @@ ggplot(beltAgeSummary, aes(x=Age.Category, y=count, fill=Belt)) +
   scale_fill_manual(values = c("#181818", "#643E2C","#7A2871","#064DAB"))
 
 
+############# Female Competitors #################
+
 
 beltAgeSexSummaryFemale =byacademy %>%
   group_by(Belt, Age.Category, Sex) %>%
@@ -35,8 +38,7 @@ ggplot(beltAgeSexSummaryFemale, aes(x=Age.Category, y=count, fill=Belt)) +
   xlab("Age Categories by Belt Rank") + ylab("Number of Atheletes") +
   geom_bar(stat='identity', width = 0.9, position = position_dodge(preserve = 'single'))+
   scale_fill_manual(values = c("#181818", "#643E2C","#7A2871","#064DAB"))
-
-#Male Athelets
+###############
 
 beltAgeSexSummaryMale =byacademy %>%
   group_by(Belt, Age.Category, Sex) %>%
@@ -53,44 +55,27 @@ ggplot(beltAgeSexSummaryMale, aes(x=Age.Category, y=count, fill=Belt)) +
   geom_bar(stat='identity', width = 0.9, position = position_dodge(preserve = 'single'))+
   scale_fill_manual(values = c("#181818", "#643E2C","#7A2871","#064DAB"))
 
-####### Top Ten Academeys
-
-topTenAcademys =byacademy %>%
+####### Academies ##############
+topTenAcademys = byacademy %>%
   group_by(Academy) %>%
   summarise(count = n(),na.rm=TRUE) %>%
   arrange(desc(count)) %>%
-  top_n(15) %>%
-
-  #filter(Sex == "Male")
-
-
-ggplot(topTenAcademys, aes(x=Academy, y=count)) +
-  geom_point(alpha=0.7)
-
-###### Bubble Charts #######
-
-sizeOfClub =byacademy %>%
-  group_by(Belt, Age.Category, Sex) %>%
-  summarise(count = n(),na.rm=TRUE) %>%
-  filter(Sex == "Male")
+  top_n(10, count)
+  
+ggplot(topTenAcademys, aes(x= reorder(Academy, -count), y=count)) +
+  ggtitle ("Top Ten Academys by Number of Students") +
+  xlab("Academys") + ylab("Number of Atheletes") +
+  geom_bar(stat='identity', width = 0.9, position = position_dodge(preserve = 'single'))
 
 
-# Most basic bubble plot
-sizeOfClub %>%
-  arrange(desc(pop)) %>%
-  mutate(country = factor(country, country)) %>%
-  ggplot(aes(x=gdpPercap, y=lifeExp, size = pop)) +
-  geom_point(alpha=0.5) +
-  scale_size(range = c(.1, 24), name="Population (M)")
-
-##### Line Graphs #####
-lineGraphs =byacademy %>%
-  group_by(Belt, Age.Category, Sex) %>%
-  summarise(count = n(),na.rm=TRUE) %>%
-  filter(Sex == "Male" & Belt == "Blue")
+###### Common Name #############
+maleFirstName = byacademy %>%
+  group_by(First.Name, Sex) %>%
+  filter(Sex == "Male") %>%
+  summarise(count = n(), na.rm=TRUE) %>%
+  arrange(desc(count)) %>%
+  top_n(15, count)
+  #top_n(1, count)
 
 
-# Plot
-  ggplot(lineGraphs, aes(x=Age.Category, y=count)) +
-  geom_line()
 
